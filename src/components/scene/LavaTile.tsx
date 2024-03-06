8c9a5505e1bb54c775b33399804e3597c9e1911f
Dainius Burokas <dainiusbur@gmail.com>
2024-03-06 13:21:59 +0200

    feat/SCRUM-10 Updated board and background models, added glow to lava

--- src/components/scene/LavaTile.tsx
+++ src/components/scene/LavaTile.tsx
@@ -1,7 +1,9 @@
 import * as THREE from 'three';
-import { useLoader } from '@react-three/fiber';
+import { useFrame, useLoader } from '@react-three/fiber';
 import imgLava from '../objects/map.png';
+import imgEmission from '../objects/emission.png';
 import { LAVA_SCALE } from './constants';
+import { useState } from 'react';
 
 interface LavaTileProps {
   position: [number, number, number];
@@ -9,10 +11,21 @@ interface LavaTileProps {
 
 const LavaTile: React.FC<LavaTileProps> = ({ position }) => {
   const base = useLoader(THREE.TextureLoader, imgLava);
+  const emission = useLoader(THREE.TextureLoader, imgEmission);
+  const [emissionMultiplier, setEmissionMultiplier] = useState(1.5);
+  useFrame((state) => {
+    setEmissionMultiplier(Math.sin(state.clock.elapsedTime) + 1);
+  });
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]} position={position}>
       <planeGeometry args={LAVA_SCALE} />
-      <meshPhysicalMaterial map={base} />
+      <meshPhysicalMaterial
+        map={base}
+        toneMapped={false}
+        emissiveMap={emission}
+        emissive={'orange'}
+        emissiveIntensity={emissionMultiplier}
+      />
     </mesh>
   );
 };
--- src/components/scene/constants.ts
+++ src/components/scene/constants.ts
@@ -1,5 +1,5 @@
 export const CAMERA_DEFAULT_POSITION: [number, number, number] = [0, 7.5, 1.2];
 export const CAMERA_TARGET_POSITION: [number, number, number] = [0, 0, 0];
-export const BOARD_STARTING_POSITION: [number, number, number] = [0, 1.8, 0];
+export const BOARD_STARTING_POSITION: [number, number, number] = [0, -3.53, 0];
 export const LAVA_STARTING_POSITION: [number, number, number] = [0, 0, 0];
 export const LAVA_SCALE: [number, number] = [30, 30];
